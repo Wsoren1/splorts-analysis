dc1e384b7312a15e58ce12de6604891c23157e6b
Wsoren1 <wsorensenm1@gmail.com>
2021-03-14 13:41:25 -0400

    changed file structure and started work on metadata handling

--- SiteManager.py
+++ SiteManager.py
@@ -1,17 +1,25 @@
 import requests as re
 import pandas as pd
 import numpy as np
+import pickle
+import atexit
 
 # Object that controls connections to sites and builds pd DataFrames
-# Using a lot of the blaseball API, doc linx https://docs.sibr.dev/docs/apis/reference/Blaseball-API.v1.yaml
+# Using a lot of the blaseball API, doc links https://docs.sibr.dev/docs/apis/reference/Blaseball-API.v1.yaml
 
 
 class SiteManager:
-    def __init__(self):
-        re.get('https://reblase.sibr.dev')
+    def __init__(self):  # TODO: assign attributes to clean up function syntax, shit's ugly af
+        self.metadata_path = r'data\metadata.pickle'
+        self.game_cache_path = r'data\game_cache.csv'
+
+        with open(self.metadata_path, "rb") as f:
+            self.metadata = pickle.load(f)
+
+        atexit.register(self.save_metadata)
 
     def get_game(self, season, day):
-        datablase = pd.read_csv("game_cache.csv")
+        datablase = pd.read_csv(self.game_cache_path)
         query = (datablase['day'] == day) & (datablase['season'] == season)
         if len(df := datablase[query]) > 0:
             if len(df) == len(df[df['finalized']]):
@@ -26,6 +34,14 @@ class SiteManager:
             datablase.to_csv("game_cache.csv", index_label=False, index=False)
             return pulled_data
 
+    def get_all_games(self):
+        df = pd.read_csv(self.game_cache_path)
+        return df[df['finalized']]
+
+    def save_metadata(self):
+        with open(self.metadata_path, 'ab') as f:
+            pickle.dump(self.metadata, f)
+
     @staticmethod
     def add_winning(df):
         if 'homeScore' not in df.keys() or 'awayScore' not in df.keys():
@@ -38,7 +54,5 @@ class SiteManager:
         df['winningTeam'] = pd.Series(results)
 
 
-
 site = SiteManager()
-print(site.get_game(2, 98))
-site.get_game(12, 58)
+site.get_game(11, 50)
